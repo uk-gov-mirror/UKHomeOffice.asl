ada1ded614fa09bef4b598389f69e6971ab14caf
Leonard Martin <leonard.martin@gmail.com>
2018-01-17 16:43:19 +0000

    Debug drone deploy vars

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,6 +3,9 @@ export KUBE_NAMESPACE=asl-dev
 export KUBE_SERVER=${KUBE_SERVER}
 export KUBE_TOKEN=${KUBE_TOKEN}
 
+echo ${DRONE_DEPLOY_TO}
+echo ${DRONE_COMMIT_SHA}
+
 kd --insecure-skip-tls-verify \
   -f deploy/deployment.yaml \
   -f deploy/service.yaml \
