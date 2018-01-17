03699eedc14b7989438bf41de4d41f9443fbdd5d
Leonard Martin <leonard.martin@gmail.com>
2018-01-17 15:40:13 +0000

    Use kd from PATH and not a local version

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,7 +3,7 @@ export KUBE_NAMESPACE=asl-dev
 export KUBE_SERVER=${KUBE_SERVER}
 export KUBE_TOKEN=${KUBE_TOKEN}
 
-./kd --insecure-skip-tls-verify \
+kd --insecure-skip-tls-verify \
   -f deployment.yaml \
   -f service.yaml \
   -f ingress.yaml
