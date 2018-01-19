7648cd8f5dba4fd7053c24800c303d008abaced3
Leonard Martin <leonard.martin@gmail.com>
2018-01-19 16:06:25 +0000

    Add keycloak auth and redis service

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const App = require('./lib/app');
 
 const settings = {
   secret: process.env.SESSION_SECRET,
-  port: process.env.PORT || 8080
+  port: process.env.PORT || 8080,
+  auth: {
+    realm: process.env.KEYCLOAK_REALM,
+    url: process.env.KEYCLOAK_URL,
+    client: process.env.KEYCLOAK_CLIENT,
+    secret: process.env.KEYCLOAK_SECRET
+  }
 };
 
 const server = App(settings).listen(settings.port, (err, result) => {
--- views/index.jsx
+++ views/index.jsx
@@ -5,7 +5,8 @@ class Index extends React.Component {
   render() {
     return (
       <Layout {...this.props}>
-        <h1 className="heading-large">Welcome</h1>
+        <h1 className="heading-large">Hello {this.props.user.firstName}</h1>
+        <p><a className="button button-large" href="/logout">Log out</a></p>
       </Layout>
     );
   }
