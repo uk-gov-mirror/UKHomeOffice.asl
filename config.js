43d3da387378aa776bb1f45bcb686aa6d961527c
Leonard Martin <leonard.martin@gmail.com>
2018-01-22 14:04:11 +0000

    Use redis as a session store

--- config.js
+++ config.js
@@ -1,6 +1,11 @@
 module.exports = {
   secret: process.env.SESSION_SECRET,
   port: process.env.PORT || 8080,
+  session: {
+    secret: process.env.SESSION_SECRET,
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT
+  },
   auth: {
     realm: process.env.KEYCLOAK_REALM,
     url: process.env.KEYCLOAK_URL,
