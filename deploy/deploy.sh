3d2292b38f883701579fefdd1d25e1d1a95f66c8
Leonard Martin <leonard.martin@gmail.com>
2018-01-26 16:47:25 +0000

    Parameterise deployment
    Makes keeping track of the names of things a bit easier when they're only defined once

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,6 +3,9 @@ export KUBE_NAMESPACE=asl-dev
 export KUBE_SERVER=${KUBE_SERVER}
 export KUBE_TOKEN=${KUBE_TOKEN}
 
+export SERVICE_NAME=app
+export DEPLOY_TARGET=${DRONE_DEPLOY_TO}
+
 kd --insecure-skip-tls-verify \
   -f deploy/app-deployment.yaml \
   -f deploy/app-service.yaml \
