0ad778e8681e6b345b475d9eb5df8ecca9b4ef6f
Leonard Martin <leonard.martin@gmail.com>
2018-03-02 11:40:44 +0000

    Simplify deployment config

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,8 +3,7 @@ export KUBE_NAMESPACE=asl-dev
 export KUBE_SERVER=${KUBE_SERVER}
 export KUBE_TOKEN=${KUBE_TOKEN}
 
-export SERVICE_NAME=app
-export DEPLOY_TARGET=${DRONE_DEPLOY_TO}
+export SERVICE_NAME=public-ui
 
 kd --insecure-skip-tls-verify \
   -f deploy/app-deployment.yaml \
