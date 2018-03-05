0818ee25bf790c7a67f9cbeb41e3dd2779a11c17
Leonard Martin <leonard.martin@gmail.com>
2018-03-05 10:43:32 +0000

    Use generated deployment scripts

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,14 +3,11 @@ export KUBE_NAMESPACE=asl-dev
 export KUBE_SERVER=${KUBE_SERVER}
 export KUBE_TOKEN=${KUBE_TOKEN}
 
-export SERVICE_NAME=public-ui
-
 kd --insecure-skip-tls-verify \
-  -f deploy/app-deployment.yaml \
-  -f deploy/app-service.yaml \
-  -f deploy/app-ingress.yaml \
+  -f deploy/deployment.yaml \
+  -f deploy/service.yaml \
+  -f deploy/ingress.yaml \
   -f deploy/network-policy.yaml \
   -f deploy/redis-deployment.yaml \
   -f deploy/redis-service.yaml \
-  -f deploy/redis-network-policy.yaml \
-  -f deploy/redirect.yaml
+  -f deploy/redis-network-policy.yaml
