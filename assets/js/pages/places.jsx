627836b8a3d5f2505f7dfa1b16d243cda482de0c
Joe Fitter <joefitter@JoesMacBookProII.local>
2018-03-27 10:27:25 +0100

    Amended webpack config to remove duplicated pages
    - removed /assets/js/pages/search|places.jsx
    - added template.jsx with {{page}} placeholder
    - traverse ./views/pages to get filenames
    - export an array of config per page
    - added string replace loader to interpolate page name into template.jsx

--- assets/js/pages/places.jsx
+++
@@ -1,5 +0,0 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
-const Component = require('../../../views/pages/places');
-
-ReactDOM.render(<Component {...window.INITIAL_STATE} />, document.getElementById('page-component'));
--- assets/js/pages/template.jsx
+++ assets/js/pages/template.jsx
@@ -1,5 +1,5 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
-const Component = require('../../../views/pages/search');
+const Component = require(`../../../views/pages/{{page}}`);
 
 ReactDOM.render(<Component {...window.INITIAL_STATE} />, document.getElementById('page-component'));
