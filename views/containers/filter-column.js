6074d92ebee8f6f424be83368b451ee85d8cfcd7
Joe Fitter <joefitter@hotmail.com>
2018-04-10 10:28:46 +0100

    Fixed lint, added tests for filter.jsx

--- views/containers/filter-column.js
+++ views/containers/filter-column.js
@@ -2,7 +2,7 @@ const { connect } = require('react-redux');
 const FilterColumn = require('../components/filter-column');
 const { toggleFilter } = require('../../src/actions/filters');
 
-const mapStateToProps = (state, { filter: { key} }) => ({
+const mapStateToProps = (state, { filter: { key } }) => ({
   filterBy: state.filters.filterBy[key]
 });
 
