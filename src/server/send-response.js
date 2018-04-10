d3818e2b32cc9621cbc7fd2d1e0c785dbcdab62d
Leonard Martin <leonard.martin@gmail.com>
2018-04-10 17:11:03 +0100

    Add csv export

--- src/server/send-response.js
+++ src/server/send-response.js
@@ -1,3 +1,5 @@
+const csv = require('csv-stringify');
+
 module.exports = () => (req, res, next) => {
 
   res.locals.store = res.store;
@@ -10,6 +12,15 @@ module.exports = () => (req, res, next) => {
         }
         throw new Error('PDF rendering is not suported for this page');
       case 'csv':
+        const list = res.store.getState().list;
+        if (list) {
+          res.type('application/csv');
+          res.attachment(`${res.template}.csv`);
+          return csv(list.filtered, { header: true }, (err, output) => {
+            err ? next(err) : res.send(output);
+          });
+        }
+        throw new Error('CSV rendering is not suported for this page');
       default:
         return res.render(res.template);
     }
