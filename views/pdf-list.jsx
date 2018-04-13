d926ed65d4ad4af7046d07df6b50fce758e8b78c
Leonard Martin <leonard.martin@gmail.com>
2018-04-13 16:38:09 +0100

    Fix PDF template
    It was referencing table components that didn't exist anymore. Update it to use the same components as the html renderer.

--- views/pdf-list.jsx
+++ views/pdf-list.jsx
@@ -1,13 +1,20 @@
-const React = require('react');
-const Pdf = require('./layouts/pdf');
-const Table = require('./components/places-table');
+import React from 'react';
+import connect from '../src/helpers/connector';
+import Pdf from './layouts/pdf';
+import SortableTable from './components/list-table';
+
+export const formatters = {
+  suitability: { format: value => value.join(', ') },
+  holding: { format: value => value.join(', ') },
+  nacwo: { format: (name, nacwo) => nacwo ? name : '-' }
+};
 
 const PdfList = props => {
-  const { hostname } = props;
+  const { hostname, list: { schema, filtered } } = props;
   const state = props.store.getState();
   return <Pdf {...state} hostname={hostname} >
-    <Table rows={ state.list.filtered } />
+    <SortableTable schema={ schema } formatters={ formatters } data={ filtered } />
   </Pdf>;
 };
 
-module.exports = PdfList;
+export default connect(PdfList, 'list');
