6cadf0dbb64495ca38d7b72b7fbb51e4024811bd
Joe Fitter <joefitter@hotmail.com>
2018-04-26 09:58:22 +0100

    get licenceHolder from establishment, not roles

--- views/index.jsx
+++ views/index.jsx
@@ -4,41 +4,46 @@ const App = require('./layouts/app');
 
 const Index = ({
   store,
-  establishment
-}) => {
-  const elh = establishment.roles.find(role => role.type === 'elh').profile;
-  const { name, licenceNumber } = establishment;
-  return (
-    <App
-      store={store}
-      crumbs={[name]}
-    >
-      <header>
-        <h2>&nbsp;</h2>
-        <h1>{ name }</h1>
-      </header>
-      <div className="grid-row">
-        <div className="column-two-thirds">
-          <ul className="dashboard">
-            <li><a href='/details'>Establishment details</a></li>
-            <li><a href='/roles'>Named people and licence holders</a></li>
-            <li><a href='/places'>Schedule of Premises</a></li>
-          </ul>
-        </div>
-        <div className="column-one-third establishment-summary">
-          <aside>
-            <dl>
-              <dt>Licence number</dt>
-              <dd>{ licenceNumber }</dd>
-
-              <dt>Licence holder</dt>
-              <dd><a href={`/profile/${elh.id}`}>{ elh.name }</a></dd>
-            </dl>
-          </aside>
-        </div>
+  establishment: {
+    name,
+    licenceNumber,
+    licenceHolder: {
+      profile: {
+        id: elhId,
+        name: elhName
+      }
+    }
+  }
+}) => (
+  <App
+    store={store}
+    crumbs={[name]}
+  >
+    <header>
+      <h2>&nbsp;</h2>
+      <h1>{ name }</h1>
+    </header>
+    <div className="grid-row">
+      <div className="column-two-thirds">
+        <ul className="dashboard">
+          <li><a href='/details'>Establishment details</a></li>
+          <li><a href='/roles'>Named people and licence holders</a></li>
+          <li><a href='/places'>Schedule of Premises</a></li>
+        </ul>
       </div>
-    </App>
-  );
-};
+      <div className="column-one-third establishment-summary">
+        <aside>
+          <dl>
+            <dt>Licence number</dt>
+            <dd>{ licenceNumber }</dd>
+
+            <dt>Licence holder</dt>
+            <dd><a href={`/profile/${elhId}`}>{ elhName }</a></dd>
+          </dl>
+        </aside>
+      </div>
+    </div>
+  </App>
+);
 
 module.exports = connect(Index);
