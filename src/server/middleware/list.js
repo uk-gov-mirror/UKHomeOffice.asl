c01204e80b5018266e323adb5c585982d825f4fb
Joe Fitter <joefitter@hotmail.com>
2018-05-01 11:04:59 +0100

    bumped asl-components to 1.2.0

--- src/server/middleware/list.js
+++ src/server/middleware/list.js
@@ -7,10 +7,8 @@ module.exports = ({ schema }) => {
   return (req, res, next) => {
     res.store.dispatch(actions.setSchema(schema));
     res.store.dispatch(actions.setListItems(res.data));
-    console.log('IN LIST MIDDLEWARE', req.query);
     if (typeof req.query.filter !== 'undefined') {
       res.store.dispatch(setTextFilter(req.query.filter));
-      console.log('FILTER', res.store.getState().filter, '**', req.query.filter);
     }
     if (typeof req.query.sort === 'object') {
       const { column, ascending } = req.query.sort;
