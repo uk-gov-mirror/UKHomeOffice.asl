fc26ffc50d2d0275a84397fc26c016f8d0fec020
Joe Fitter <joefitter@hotmail.com>
2018-05-18 10:34:36 +0100

    remove all the code
    fix lint
    
    removed compile step

--- src/helpers/connector.js
+++
@@ -1,7 +0,0 @@
-const { connect } = require('react-redux');
-const { pick } = require('lodash');
-
-module.exports = (Component, ...keys) => {
-  keys.push('establishment');
-  return connect(state => pick(state, keys))(Component);
-};
