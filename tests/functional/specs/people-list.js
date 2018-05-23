af00c604f231bfddb8f366ca1e6890ab8de34f64
Leonard Martin <leonard.martin@gmail.com>
2018-05-23 11:59:33 +0100

    Add functional tests for people page
    This tests that the page level filtering works correctly.

---
+++ tests/functional/specs/people-list.js
@@ -0,0 +1,49 @@
+const assert = require('assert');
+
+describe('People directory', () => {
+
+  it('will filter to a particular role', () => {
+    browser.withUser('holc');
+    browser.click('a[href*="/people"]');
+
+    browser.$('.link-filter').$('a=NACWO').click();
+
+    const roles = browser.$$('tbody tr td:nth-child(2)')
+      .map(td => browser.elementIdText(td.ELEMENT).value);
+
+    roles.forEach(role => assert.ok(role.includes('NACWO')));
+  });
+
+  it('will filter on the name', () => {
+    browser.withUser('holc');
+    browser.click('a[href*="/people"]');
+
+    browser.$('.search-box input[type="text"]').setValue('Laur');
+    browser.$('.search-box button').click();
+
+    const names = browser.$$('tbody tr td:nth-child(1)')
+      .map(td => browser.elementIdText(td.ELEMENT).value);
+
+    names.forEach(name => assert.ok(name.includes('Laur')));
+  });
+
+  it('will filter to a particular role and name', () => {
+    browser.withUser('holc');
+    browser.click('a[href*="/people"]');
+
+    browser.$('.link-filter').$('a=NACWO').click();
+
+    browser.$('.search-box input[type="text"]').setValue('b');
+    browser.$('.search-box button').click();
+
+    const roles = browser.$$('tbody tr td:nth-child(2)')
+      .map(td => browser.elementIdText(td.ELEMENT).value);
+
+    const names = browser.$$('tbody tr td:nth-child(1)')
+      .map(td => browser.elementIdText(td.ELEMENT).value);
+
+    roles.forEach(role => assert.ok(role.includes('NACWO')));
+    names.forEach(name => assert.ok(name.toLowerCase().includes('b')));
+  });
+
+});
