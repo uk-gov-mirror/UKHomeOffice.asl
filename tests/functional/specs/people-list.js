fb69b7df21078d07a17aec075a86ea3aa76dd0f0
Elvin Ali <ali.elvin@gmail.com>
2018-08-31 16:40:43 +0100

    Use single quotes

--- tests/functional/specs/people-list.js
+++ tests/functional/specs/people-list.js
@@ -1,69 +1,69 @@
-const assert = require("assert");
+const assert = require('assert');
 
-describe("People directory", () => {
-  it("will filter to a particular role", () => {
-    browser.withUser("holc");
-    browser.click('a=University of Croydon').click("a=People");
-
-    browser
-      .$(".link-filter")
-      .$("a=NACWO")
-      .click();
-
-    browser.waitForExist("table:not(.loading)");
-
-    const roles = browser
-      .$$("tbody tr td:nth-child(2)")
-      .map(td => browser.elementIdText(td.ELEMENT).value);
-
-    roles.forEach(role => assert.ok(role.includes("NACWO")));
-  });
-
-  it("will filter on the name", () => {
-    browser.withUser("holc");
-    browser.click('a=University of Croydon').click("a=People");
-
-    browser.$('.search-box input[type="text"]').setValue("Laur");
-    browser.$(".search-box button").click();
-
-    browser.waitForExist("table:not(.loading)");
-
-    const names = browser
-      .$$("tbody tr td:nth-child(1)")
-      .map(td => browser.elementIdText(td.ELEMENT).value);
-
-    names.forEach(name => assert.ok(name.includes("Laur")));
-  });
-
-  it("will filter to a particular role and name", () => {
-    browser.withUser("holc");
-    browser.click('a=University of Croydon').click("a=People");
+describe('People directory', () => {
+  it('will filter to a particular role', () => {
+    browser.withUser('holc');
+    browser.click('a=University of Croydon').click('a=People');
 
     browser
-      .$(".link-filter")
-      .$("a=NACWO")
+      .$('.link-filter')
+      .$('a=NACWO')
       .click();
 
-    browser.waitForExist("table:not(.loading)");
+    browser.waitForExist('table:not(.loading)');
 
-    browser.$('.search-box input[type="text"]').setValue("b");
-    browser.$(".search-box button").click();
+    const roles = browser
+      .$$('tbody tr td:nth-child(2)')
+      .map(td => browser.elementIdText(td.ELEMENT).value);
 
-    browser.waitForExist("table:not(.loading)");
+    roles.forEach(role => assert.ok(role.includes('NACWO')));
+  });
+
+  it('will filter on the name', () => {
+    browser.withUser('holc');
+    browser.click('a=University of Croydon').click('a=People');
+
+    browser.$('.search-box input[type="text"]').setValue('Laur');
+    browser.$('.search-box button').click();
+
+    browser.waitForExist('table:not(.loading)');
 
     const names = browser
-      .$$("tbody tr td:nth-child(1)")
+      .$$('tbody tr td:nth-child(1)')
+      .map(td => browser.elementIdText(td.ELEMENT).value);
+
+    names.forEach(name => assert.ok(name.includes('Laur')));
+  });
+
+  it('will filter to a particular role and name', () => {
+    browser.withUser('holc');
+    browser.click('a=University of Croydon').click('a=People');
+
+    browser
+      .$('.link-filter')
+      .$('a=NACWO')
+      .click();
+
+    browser.waitForExist('table:not(.loading)');
+
+    browser.$('.search-box input[type="text"]').setValue('b');
+    browser.$('.search-box button').click();
+
+    browser.waitForExist('table:not(.loading)');
+
+    const names = browser
+      .$$('tbody tr td:nth-child(1)')
       .map(td => browser.elementIdText(td.ELEMENT).value);
 
     const roles = browser
-      .$$("tbody tr td:nth-child(2)")
+      .$$('tbody tr td:nth-child(2)')
       .map(td => browser.elementIdText(td.ELEMENT).value);
 
     roles.forEach(role =>
-      assert.ok(role.includes("NACWO"), `${role} should contain "NACWO"`)
+      assert.ok(role.includes('NACWO'), `${role} should contain 'NACWO'`)
     );
     names.forEach(name =>
-      assert.ok(name.toLowerCase().includes("b"), `${name} should contain "b"`)
+      assert.ok(name.toLowerCase().includes('b'), `${name} should contain 'b'`)
     );
   });
 });
--- tests/functional/specs/projects-list.js
+++ tests/functional/specs/projects-list.js
@@ -1,53 +1,53 @@
-const assert = require("assert");
+const assert = require('assert');
 
-describe("Projects directory", () => {
-  it("will filter by project title", () => {
-    browser.withUser("holc");
-    browser.click("a=University of Croydon").click("a=Projects");
+describe('Projects directory', () => {
+  it('will filter by project title', () => {
+    browser.withUser('holc');
+    browser.click('a=University of Croydon').click('a=Projects');
 
-    browser.$('.search-box input[type="text"]').setValue("anti-cancer");
-    browser.$(".search-box button").click();
+    browser.$('.search-box input[type="text"]').setValue('anti-cancer');
+    browser.$('.search-box button').click();
 
-    browser.waitForExist("table:not(.loading)");
+    browser.waitForExist('table:not(.loading)');
 
     const projects = browser
-      .$$("tbody tr td:nth-child(0)")
+      .$$('tbody tr td:nth-child(0)')
       .map(td => browser.elementIdText(td.ELEMENT).value);
 
     projects.forEach(project =>
-      assert.ok(project.toLowerCase().includes("anti-cancer"))
+      assert.ok(project.toLowerCase().includes('anti-cancer'))
     );
   });
 
-  it("will filter by license holder", () => {
-    browser.withUser("holc");
-    browser.click("a=University of Croydon").click("a=Projects");
+  it('will filter by license holder', () => {
+    browser.withUser('holc');
+    browser.click('a=University of Croydon').click('a=Projects');
 
-    browser.$('.search-box input[type="text"]').setValue("br");
-    browser.$(".search-box button").click();
+    browser.$('.search-box input[type="text"]').setValue('br');
+    browser.$('.search-box button').click();
 
-    browser.waitForExist("table:not(.loading)");
+    browser.waitForExist('table:not(.loading)');
 
     const holders = browser
-      .$$("tbody tr td:nth-child(2)")
+      .$$('tbody tr td:nth-child(2)')
       .map(td => browser.elementIdText(td.ELEMENT).value);
 
-    holders.forEach(holder => assert.ok(holder.toLowerCase().includes("br")));
+    holders.forEach(holder => assert.ok(holder.toLowerCase().includes('br')));
   });
 
-  it("will filter by license number", () => {
-    browser.withUser("holc");
-    browser.click("a=University of Croydon").click("a=Projects");
+  it('will filter by license number', () => {
+    browser.withUser('holc');
+    browser.click('a=University of Croydon').click('a=Projects');
 
-    browser.$('.search-box input[type="text"]').setValue("1");
-    browser.$(".search-box button").click();
+    browser.$('.search-box input[type="text"]').setValue('1');
+    browser.$('.search-box button').click();
 
-    browser.waitForExist("table:not(.loading)");
+    browser.waitForExist('table:not(.loading)');
 
     const numbers = browser
-      .$$("tbody tr td:nth-child(3)")
+      .$$('tbody tr td:nth-child(3)')
       .map(td => browser.elementIdText(td.ELEMENT).value);
 
-    numbers.forEach(holder => assert.ok(holder.toLowerCase().includes("1")));
+    numbers.forEach(holder => assert.ok(holder.toLowerCase().includes('1')));
   });
 });
