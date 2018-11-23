9905fa1d86102d3fed2120211d4a4fecf9e5bd4b
Leonard Martin <leonard.martin@gmail.com>
2018-11-23 17:19:09 +0000

    Move dashboard into this project and add invitations
    There's no reason for the external user's dashboard to be in @asl/pages, since it isn't used anyhwere except here.
    
    Also, add a section on the user's dashboard that shows any pending invitations for that user's email address.

---
+++ pages/dashboard/content/index.js
@@ -0,0 +1,23 @@
+module.exports = {
+  tasklist: {
+    title: 'Tasks',
+    outstanding: {
+      none: 'You have no outstanding tasks',
+      some: 'You have {{count}} outstanding tasks'
+    }
+  },
+  fields: {
+    updatedAt: {
+      label: 'Received'
+    },
+    establishment: {
+      label: 'Establishment'
+    },
+    licence: {
+      label: 'Licence'
+    },
+    type: {
+      label: 'Type'
+    }
+  }
+};
