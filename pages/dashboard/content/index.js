40fa5abefd5658dc2cf2a363d1c4a63449be3902
Elvin Ali <ali.elvin@gmail.com>
2019-02-22 12:37:30 +0000

    Use component Profile to display profile

--- pages/dashboard/content/index.js
+++ pages/dashboard/content/index.js
@@ -9,16 +9,16 @@ module.exports = merge({}, baseContent, {
       some: 'You have {{count}} outstanding tasks'
     }
   },
-  projects: {
-    title: 'Active Projects',
-    projectTitles: 'Project titles',
-    licenceNumber: 'Licence number: {{licenceNumber}}',
-    expiryDate: 'Expiry date: {{expiryDate}}',
-    noProjects: 'You don\'t have any active projects'
-  },
   responsibilities: {
     title: 'Roles',
-    noRoles: 'You don\'t have any named roles'
+    noRoles: "You don't have any named roles",
+    roleApply: 'Apply for named role'
+  },
+  projects: {
+    title: 'Active Projects',
+    licenceNumber: 'Licence number: {{licenceNumber}}',
+    expiryDate: 'Expiry date: {{expiryDate}}',
+    noProjects: "You don't have any active projects"
   },
   contactDetails: {
     title: 'Contact Details',
@@ -26,13 +26,25 @@ module.exports = merge({}, baseContent, {
     telephone: 'Telephone',
     email: 'Email Address'
   },
-  pil: {
-    title: 'Personal Licence',
-    noPil: 'You don\'t have a personal licence (PIL)'
+  permissionLevel: {
+    title: 'Permission level'
   },
   buttons: {
+    continue: 'Continue',
     pplApply: 'Apply for project licence',
     pilApply: 'Apply for personal licence',
     roleApply: 'Apply for named role'
+  },
+  pil: {
+    title: 'Personal Licence',
+    noPil: "You don't have a personal licence (PIL)",
+    user: {
+      incomplete: 'You have an incomplete PIL application.',
+      notStarted: "You don't hold a personal (PIL) licence."
+    },
+    other: {
+      incomplete: 'Incomplete PIL application.',
+      notStarted: 'No personal (PIL) licence held.'
+    }
   }
 });
--- pages/dashboard/views/index.jsx
+++ pages/dashboard/views/index.jsx
@@ -8,10 +8,8 @@ import {
   ExpandingPanel
 } from '@asl/components';
 import TaskList from '@asl/pages/pages/task/list/views/tasklist';
-import format from 'date-fns/format';
-import { isEmpty, sortBy } from 'lodash';
-import dict from '@asl/dictionary';
-import { Button } from '@ukhomeoffice/react-components';
+import sortBy from 'lodash/sortBy';
+import Profile from '@asl/pages/pages/profile/read/views/profile';
 
 const Invitation = ({ token, establishment }) => (
   <Fragment>
@@ -21,171 +19,37 @@ const Invitation = ({ token, establishment }) => (
   </Fragment>
 );
 
-const defineValue = val => `${dict[val.toUpperCase()]} (${val})`;
-
 const Index = ({
   tabs,
   progress,
-  profile: {
-    firstName,
-    establishments,
-    invitations,
-    projects,
-    roles,
-    pil,
-    address,
-    telephone,
-    email,
-    postcode,
-    allowedActions,
-    id
-  }
+  profile
 }) => {
 
   return <Fragment>
     <Header
-      title={<Snippet name={firstName}>pages.dashboard.greeting</Snippet>}
+      title={<Snippet name={profile.firstName}>pages.dashboard.greeting</Snippet>}
     />
     <div className="govuk-grid-row">
       <div className="govuk-grid-column-full">
         <h2><Snippet>pages.dashboard.tasks</Snippet></h2>
         <TaskList tabs={ tabs } progress={ progress } />
         {
-          !!establishments.length && <Fragment>
+          !!profile.establishments.length && <Fragment>
             <h2>Establishments</h2>
-            <PanelList panels={sortBy(establishments, 'name').map(establishment => {
+            <PanelList panels={sortBy(profile.establishments, 'name').map(establishment => {
 
               return (
                 <ExpandingPanel key={establishment.id} title={establishment.name}>
-                  <p><Link page="establishment.dashboard" establishmentId={establishment.id} label='About this establishment' /></p>
-                  <div className='separator' />
-
-                  <h3><Snippet>projects.title</Snippet></h3>
-
-                  <Fragment>
-                    {
-                      projects && !isEmpty(projects.filter(({ establishmentId }) => establishmentId === establishment.id)) && (
-                        <dl>
-                          {
-                            projects.filter(({ establishmentId }) => establishmentId === establishment.id).map(project =>
-                              <Fragment key={project.id}>
-                                <dd>
-                                  <Link page="project.list" establishmentId={establishment.id} label={project.title} />
-                                </dd>
-                                <dd>
-                                  <span><Snippet licenceNumber={project.licenceNumber}>projects.licenceNumber</Snippet></span>
-                                </dd>
-                                <dd>
-                                  <span><Snippet expiryDate={format(project.expiryDate, 'DD MMMM YYYY')}>projects.expiryDate</Snippet></span>
-                                </dd>
-                              </Fragment>
-                            )
-                          }
-                        </dl>
-                      )
-                    }
-                    {
-                      projects && isEmpty(projects.filter(({ establishmentId }) => establishmentId === establishment.id)) &&
-                        <p><Snippet>projects.noProjects</Snippet></p>
-                    }
-                    {
-                      allowedActions[establishment.id].includes('project.apply') && (
-                        <form method="POST" action={`/e/${establishment.id}/projects/create`}>
-                          <Button className="govuk-button add-margin"><Snippet>buttons.pplApply</Snippet></Button>
-                        </form>
-                      )
-                    }
-                  </Fragment>
-                  <div className='separator' />
-
-                  <h3><Snippet>responsibilities.title</Snippet></h3>
-                  <Fragment>
-                    {!isEmpty(roles.filter(({ establishmentId }) => establishmentId === establishment.id)) &&
-                      <dl>
-                        {
-                          roles.filter(({ establishmentId }) => establishmentId === establishment.id).map(({ type, id }) => <Fragment key={id}>
-                            <dd>{defineValue(type.toUpperCase())}</dd>
-                          </Fragment>)
-                        }
-                      </dl>
-                    }
-                    {
-                      isEmpty(roles.filter(({ establishmentId }) => establishmentId === establishment.id)) && <Snippet>responsibilities.noRoles</Snippet>
-                    }
-                    {
-                      <p><Link
-                        page='profile.role.apply.base' establishmentId={establishment.id} profileId={id}
-                        className="govuk-button"
-                        label={<Snippet>{`buttons.roleApply`}</Snippet>}
-                      /></p>
-                    }
-                  </Fragment>
-
-                  <div className='separator' />
-
-                  <h3><Snippet>pil.title</Snippet></h3>
-                  <Fragment>
-                    {
-                      pil && pil.licenceNumber && (
-                        <dl>
-                          <dt><Snippet>personalLicenceNumber</Snippet></dt>
-                          <dd><Link page="pil.read" establishmentId={establishment.id} profileId={id} pilId={pil.id} label={pil.licenceNumber} /></dd>
-                        </dl>
-                      )
-                    }
-                    { !pil && <p className="add-margin"><Snippet>pil.noPil</Snippet></p> }
-                    { !pil && allowedActions[establishment.id].includes('pil.create') &&
-                        <p><Link
-                          page='pil.create' establishmentId={establishment.id} profileId={id}
-                          className="govuk-button"
-                          label={<Snippet>{`buttons.pilApply`}</Snippet>}
-                        /></p>
-                    }
-                  </Fragment>
-                  <div className='separator' />
-
-                  <h3><Snippet>contactDetails.title</Snippet></h3>
-                  {
-                    (address || telephone || email) && (
-                      <Fragment>
-                        <dl>
-                          {
-                            email && (
-                              <Fragment>
-                                <dt><Snippet>contactDetails.email</Snippet></dt>
-                                <dd><a href={`mailto:${email}`}>{email}</a></dd>
-                              </Fragment>
-                            )
-                          }
-                          {
-                            address && (
-                              <Fragment>
-                                <dt><Snippet>contactDetails.professionalAddress</Snippet></dt>
-                                <dd>{address}<br />{postcode}</dd>
-                              </Fragment>
-                            )
-                          }
-                          {
-                            telephone && (
-                              <Fragment>
-                                <dt><Snippet>contactDetails.telephone</Snippet></dt>
-                                <dd>{telephone}</dd>
-                              </Fragment>
-                            )
-                          }
-                        </dl>
-                      </Fragment>
-                    )
-                  }
-
-                </ExpandingPanel>);
+                  <Profile establishment={establishment} profile={profile} allowedActions={profile.allowedActions[establishment.id]}/>
+                </ExpandingPanel>
+              );
             })} />
           </Fragment>
         }
         {
-          !!invitations.length && <Fragment>
+          !!profile.invitations.length && <Fragment>
             <h2>Pending Invitations</h2>
-            <PanelList panels={invitations.map(invitation => <Invitation key={invitation.id} establishment={ invitation.establishment.name } token={invitation.token} />)}/>
+            <PanelList panels={profile.invitations.map(invitation => <Invitation key={invitation.id} establishment={ invitation.establishment.name } token={invitation.token} />)}/>
           </Fragment>
         }
       </div>
@@ -193,5 +57,5 @@ const Index = ({
   </Fragment>;
 };
 
-const mapStateToProps = ({ static: { profile, tabs, progress, isUser, allowedActions } }) => ({ profile, tabs, progress, isUser, allowedActions });
+const mapStateToProps = ({ static: { profile, tabs, progress, isUser } }) => ({ profile, tabs, progress, isUser });
 export default connect(mapStateToProps)(Index);
