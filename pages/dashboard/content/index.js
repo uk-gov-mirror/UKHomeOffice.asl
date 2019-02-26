46f20d1bc4320b5918497ce901837935d716b50d
Elvin Ali <ali.elvin@gmail.com>
2019-02-26 16:26:28 +0000

    Add .landing style, minor release, import content from asl-pages, organise imports, remove unnecessary divs

--- pages/dashboard/content/index.js
+++ pages/dashboard/content/index.js
@@ -1,50 +1,13 @@
 const { merge } = require('lodash');
 const baseContent = require('@asl/pages/pages/task/list/content');
+const profileContent = require('@asl/pages/pages/profile/read/content');
 
-module.exports = merge({}, baseContent, {
+module.exports = merge({}, baseContent, profileContent, {
   tasklist: {
     title: 'Tasks',
     outstanding: {
       none: 'You have no outstanding tasks',
       some: 'You have {{count}} outstanding tasks'
-    }
-  },
-  responsibilities: {
-    title: 'Roles',
-    noRoles: "You don't have any named roles",
-    roleApply: 'Apply for named role'
-  },
-  projects: {
-    title: 'Active Projects',
-    licenceNumber: 'Licence number: {{licenceNumber}}',
-    expiryDate: 'Expiry date: {{expiryDate}}',
-    noProjects: "You don't have any active projects"
-  },
-  contactDetails: {
-    title: 'Contact Details',
-    professionalAddress: 'Professional Address',
-    telephone: 'Telephone',
-    email: 'Email Address'
-  },
-  permissionLevel: {
-    title: 'Permission level'
-  },
-  buttons: {
-    continue: 'Continue',
-    pplApply: 'Apply for project licence',
-    pilApply: 'Apply for personal licence',
-    roleApply: 'Apply for named role'
-  },
-  pil: {
-    title: 'Personal Licence',
-    noPil: "You don't have a personal licence (PIL)",
-    user: {
-      incomplete: 'You have an incomplete PIL application.',
-      notStarted: "You don't hold a personal (PIL) licence."
-    },
-    other: {
-      incomplete: 'Incomplete PIL application.',
-      notStarted: 'No personal (PIL) licence held.'
     }
   }
 });
--- pages/dashboard/views/index.jsx
+++ pages/dashboard/views/index.jsx
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
+import sortBy from 'lodash/sortBy';
 import {
   Link,
   Snippet,
@@ -8,7 +9,6 @@ import {
   ExpandingPanel
 } from '@asl/components';
 import TaskList from '@asl/pages/pages/task/list/views/tasklist';
-import sortBy from 'lodash/sortBy';
 import Profile from '@asl/pages/pages/profile/read/views/profile';
 
 const Invitation = ({ token, establishment }) => (
@@ -29,30 +29,30 @@ const Index = ({
     <Header
       title={<Snippet name={profile.firstName}>pages.dashboard.greeting</Snippet>}
     />
-    <div className="govuk-grid-row">
-      <div className="govuk-grid-column-full">
-        <h3><Snippet>pages.dashboard.tasks</Snippet></h3>
-        <TaskList tabs={ tabs } progress={ progress } />
-        {
-          !!profile.establishments.length && <Fragment>
-            <h3>Establishments</h3>
-            <PanelList panels={sortBy(profile.establishments, 'name').map(establishment => {
-              return (
-                <ExpandingPanel key={establishment.id} title={establishment.name}>
-                  <Profile establishment={establishment} profile={profile} allowedActions={profile.allowedActions[establishment.id]}/>
-                </ExpandingPanel>
-              );
-            })} />
-          </Fragment>
-        }
-        {
-          !!profile.invitations.length && <Fragment>
-            <h3>Pending Invitations</h3>
-            <PanelList panels={profile.invitations.map(invitation => <Invitation key={invitation.id} establishment={ invitation.establishment.name } token={invitation.token} />)}/>
-          </Fragment>
-        }
-      </div>
-    </div>
+
+    <h3><Snippet>pages.dashboard.tasks</Snippet></h3>
+    <TaskList tabs={ tabs } progress={ progress } />
+    {
+      !!profile.establishments.length && <Fragment>
+        <h3>Establishments</h3>
+        <PanelList panels={sortBy(profile.establishments, 'name').map(establishment => {
+          return (
+            <div className="landing" key={establishment.id}>
+              <ExpandingPanel title={establishment.name}>
+                <Profile establishment={establishment} profile={profile} allowedActions={profile.allowedActions[establishment.id]}/>
+              </ExpandingPanel>
+            </div>
+          );
+        })} />
+      </Fragment>
+    }
+    {
+      !!profile.invitations.length && <Fragment>
+        <h3>Pending Invitations</h3>
+        <PanelList panels={profile.invitations.map(invitation => <Invitation key={invitation.id} establishment={ invitation.establishment.name } token={invitation.token} />)}/>
+      </Fragment>
+    }
+
   </Fragment>;
 };
 
