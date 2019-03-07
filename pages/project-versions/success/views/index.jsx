4f646a795fe52cbcdb8324bbfc313a153d3780e3
Joe Fitter <joefitter@hotmail.com>
2019-03-07 10:37:41 +0000

    bumped pages, use success page from pages

--- pages/project-versions/success/views/index.jsx
+++ pages/project-versions/success/views/index.jsx
@@ -1,12 +1,3 @@
-import React from 'react';
-import { Snippet, Panel } from '@asl/components';
-
-const Success = () => (
-  <div className="govuk-grid-row">
-    <div className="govuk-grid-column-two-thirds">
-      <Panel title={<Snippet>title</Snippet>} className="green-bg" />
-    </div>
-  </div>
-);
+import Success from '@asl/pages/pages/common/views/success';
 
 export default Success;
