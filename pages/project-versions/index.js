be6f5f3f3a6c967d96a0835fa8607db5dcb4f919
Joe Fitter <joefitter@hotmail.com>
2019-04-12 12:54:17 +0100

    moved getVersion into router, added notes, bumped pages

--- pages/project-versions/index.js
+++ pages/project-versions/index.js
@@ -2,12 +2,19 @@ const { Router } = require('express');
 const update = require('./update');
 const submit = require('./submit');
 const success = require('./success');
+const { getVersion } = require('@asl/pages/pages/project-version/middleware');
+console.log(getVersion.toString())
 
 module.exports = () => {
   const app = Router();
 
+  app.use(getVersion());
+
   app.use('/submit', submit());
   app.use('/success', success());
+
+  // we always want to serve the same template and
+  // scripts for any sub-routes under /edit
   app.use('/*', update());
 
   app.use((req, res) => res.sendResponse());
