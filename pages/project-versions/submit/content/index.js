4b182fb53597d6df947dce44ae6f9cf39b28a2ea
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2019-07-25 17:07:58 +0100

    add extra capture fields to project declarations

--- pages/project-versions/submit/content/index.js
+++ pages/project-versions/submit/content/index.js
@@ -3,13 +3,26 @@ module.exports = {
   warning: 'Project licence applications need to be reviewed by an AWERB and endorsed by the primary establishment\'s PEL holder before a licence can be granted.',
   fields: {
     authority: {
-      label: 'Does this {{model.type}} have the endorsement of your primary establishment\'s PEL holder?'
+      label: 'Does this {{model.type}} have the endorsement of your primary establishment\'s PEL holder?',
+      hint: 'The Home Office may be in touch with your establishment to collect evidence of this endorsement.'
+    },
+    'authority-pelholder-name': {
+      label: 'Please state the name of your primary establishment’s PEL holder'
+    },
+    'authority-endorsement-date': {
+      label: 'Please state the date of their endorsement'
     },
     awerb: {
-      label: 'Has this application been reviewed by the AWERB of each relevant establishment?'
+      label: 'Has this {{model.type}} been reviewed by the AWERB of each relevant establishment?'
+    },
+    'awerb-review-date': {
+      label: 'Please state the date of the most recent AWERB review for each relevant establishment.'
+    },
+    'awerb-no-review-reason': {
+      label: 'Why has this amendment not been reviewed by the AWERB of each relevant establishment?'
     },
     ready: {
-      label: 'Is this version of your application ready for an inspector to approve?'
+      label: 'Has an inspector advised you that this version of your draft application is ready for assessment?'
     },
     comment: {
       label: 'Why are you making this amendment?'
@@ -19,8 +32,20 @@ module.exports = {
     authority: {
       required: 'Select an option'
     },
+    'authority-pelholder-name': {
+      required: 'Please provide the name of the PEL holder'
+    },
+    'authority-endorsement-date': {
+      required: 'Please provide the endorsement date'
+    },
     awerb: {
       required: 'Select an option'
+    },
+    'awerb-review-date': {
+      required: 'Please provide the AWERB review date(s)'
+    },
+    'awerb-no-review-reason': {
+      required: 'Please explain why there was no AWERB review'
     },
     ready: {
       required: 'Select an option'
