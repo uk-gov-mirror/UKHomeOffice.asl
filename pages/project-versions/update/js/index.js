15d5e40fe373778b88f36c8dc31f29dd150bd012
Joe Fitter <joefitter@hotmail.com>
2019-08-13 09:51:09 +0100

    Improve project sync
    * removed updateProject action - now lives in projects
    * simplified init, now just takes initialState
    * bumped projects and pages

--- pages/project-versions/update/js/index.js
+++ pages/project-versions/update/js/index.js
@@ -1,77 +1,9 @@
 import start from '@asl/projects';
-import { throwError } from '@asl/projects/client/actions/messages';
-import { updateSavedProject } from '@asl/projects/client/actions/projects';
-import debounce from 'lodash/debounce';
-import fetch from 'r2';
 import cloneDeep from 'lodash/cloneDeep';
-import { diff, applyChange } from 'deep-diff';
-
-const updateProject = project => {
-  return {
-    type: 'UPDATE_PROJECT',
-    project
-  };
-};
-
-const applyPatches = (source, patches = []) => {
-  const patched = cloneDeep(source);
-  patches.forEach(p => {
-    applyChange(patched, p);
-  });
-  return patched;
-};
 
 const state = window.INITIAL_STATE;
 
-const postData = debounce((patch, getState, dispatch) => {
-  fetch(state.static.basename, {
-    method: 'PUT',
-    credentials: 'include',
-    json: patch
-  })
-    .response
-    .then(response => {
-      return response.json()
-        .then(json => {
-          if (response.status > 399) {
-            const err = new Error(json.message || `Fetch failed with status code: ${response.status}`);
-            err.status = response.status;
-            Object.assign(err, json);
-            throw err;
-          }
-        })
-        .then(() => {
-          const { savedProject } = getState();
-          const patched = applyPatches(savedProject, patch);
-          dispatch(updateSavedProject(patched));
-        });
-    })
-    .catch(err => {
-      console.error(err);
-      dispatch(throwError('data sync failed, please try again'));
-    });
-}, 500, { maxWait: 5000 });
-
-const onUpdate = props => {
-  return (dispatch, getState) => {
-    const { project, savedProject } = getState();
-    const newState = { ...project, ...props };
-
-    dispatch(updateProject(newState));
-    const patch = diff(savedProject, newState);
-    return postData(patch, getState, dispatch);
-  };
-};
-
-const onComplete = () => {
-  window.location.href = `${state.static.basename}/submit`;
-};
-
 start({
-  basename: state.static.basename,
-  onUpdate,
-  onComplete
-}, {
   project: {
     ...state.model.data,
     id: state.model.id
