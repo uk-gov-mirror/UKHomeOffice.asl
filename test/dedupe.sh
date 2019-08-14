18679af29c990166933315c3629c63bc087a2eec
Joe Fitter <joefitter@hotmail.com>
2019-08-14 15:53:37 +0100

    updated to remove loop and log path

--- test/dedupe.sh
+++ test/dedupe.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-REACT_REDUX_PATH=./node_modules/@asl/*/node_modules/react-redux/package.json
-REACT_PATH=./node_modules/@asl/*/node_modules/react/package.json
+REACT_REDUX_PATH="$(find . | grep ./node_modules/@asl/*/node_modules/react-redux/package.json)"
+REACT_PATH="$(find . | grep ./node_modules/@asl/*/node_modules/react/package.json)"
 
-PATHS=($REACT_PATH $REACT_REDUX_PATH)
+if [ ! -z $REACT_PATH ]; then
+  echo $REACT_PATH
+  echo "Found multiple versions of react, run npm dedupe"
+  exit 1
+fi
 
-for i in ${PATHS[@]}; do
-  FOUND="$(find . | egrep $i | wc -l)"
-  if [ $FOUND -gt 0 ]; then
-    exit 1
-  fi
-done
+if [ ! -z $REACT_REDUX_PATH ]; then
+  echo $REACT_REDUX_PATH
+  echo "Found multiple versions of react-redux, run npm dedupe"
+  exit 1
+fi
+
 exit 0
