48a0e646b39b5ff07efcbcd1a5c06f2611004e4f
Leonard Martin <leonard.martin@gmail.com>
2019-09-26 17:41:28 +0100

    Dedupe react-router

--- test/dedupe.sh
+++ test/dedupe.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 REACT_REDUX_PATH="$(find . | grep ./node_modules/@asl/*/node_modules/react-redux/package.json)"
+REACT_ROUTER_PATH="$(find . | grep ./node_modules/@asl/*/node_modules/react-router/package.json)"
 REACT_PATH="$(find . | grep ./node_modules/@asl/*/node_modules/react/package.json)"
 
 if [ ! -z $REACT_PATH ]; then
@@ -11,6 +12,12 @@ fi
 if [ ! -z $REACT_REDUX_PATH ]; then
   echo $REACT_REDUX_PATH
   echo "Found multiple versions of react-redux, run npm dedupe"
+  exit 1
+fi
+
+if [ ! -z $REACT_ROUTER_PATH ]; then
+  echo $REACT_ROUTER_PATH
+  echo "Found multiple versions of react-router, run npm dedupe"
   exit 1
 fi
 
