aac2a55d14ed0f3dfeb1af8f1b347dd3971da984
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2020-09-07 17:36:15 +0100

    add contact us page

---
+++ pages/contact/content/index.js
@@ -0,0 +1,30 @@
+module.exports = {
+  breadcrumbs: {
+    contactUs: 'Contact us'
+  },
+  content: `
+    # Contact us
+    Please note our working hours are Monday to Friday, 9am to 5pm (except public holidays).
+
+    We aim to respond to emails within 3 working days.
+
+    ## Technical issues
+    For help using this website, email [aspelqueries@homeoffice.gov.uk](mailto:aspelqueries@homeoffice.gov.uk) or call
+    020 7035 4469.
+
+    [Find out about call charges](https://www.gov.uk/call-charges).
+
+    ## Licensing
+    For help with licences, contact your establishment's Single Point of Contact, details of which can be found on your
+    establishment’s home page. You can also email [aspa.london@homeoffice.gov.uk](mailto:aspa.london@homeoffice.gov.uk).
+
+    ## Billing
+    For help with invoicing and payments, email the business support team at
+    [asrubusiness@homeoffice.gov.uk](mailto:asrubusiness@homeoffice.gov.uk).
+
+
+    ## Newsletters
+    If you have questions about the Animals in Science Regulation Unit’s
+    [newsletters](https://www.gov.uk/government/publications/animals-in-science-regulation-unit-newsletters), email
+    [asrubusiness@homeoffice.gov.uk](mailto:asrubusiness@homeoffice.gov.uk).`
+};
