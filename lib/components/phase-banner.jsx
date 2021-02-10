9b8ff81fd8a70e7204425980e059df2169f7feac
Leonard Martin <leonard.martin@gmail.com>
2021-02-10 11:53:56 +0000

    Add custom phase banner content with link to RoPs survey
    Defines a local override of the default phase banner content to include link to RoPs survey.

---
+++ lib/components/phase-banner.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const PhaseBanner = () => <span><a href="https://www.homeofficesurveys.homeoffice.gov.uk/s/U8PEWV/" rel="noopener noreferrer" target="_blank">Give feedback on the 2020 return of procedures process</a> so we can improve it next year.</span>;
+
+export default PhaseBanner;
